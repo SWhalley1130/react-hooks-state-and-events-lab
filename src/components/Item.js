8cf5f414469c34f905f4784e05153883e4ef71d5
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-22 15:21:28 -0700

    Adds ability to check off/ readd items to grocery list

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [className, setClassName]=useState("");
+
+  function handleCartButton()
+  {
+    if (className==="")
+    {
+      setClassName("in-cart");
+    }
+    else {setClassName("")}
+  }
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartButton}>Add to Cart</button>
     </li>
   );
 }
